603b32d2b7299412ebf417f500ec497b96b8b7e7
AdemOkurDE <ademokur.tr@gmail.com>
2023-07-25 10:19:04 +0200

    first commit

---
+++ src/test/java/tests/US01_TC001.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class US01_TC001 {
+}
+/*
+echo "# TestNG_Project01" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AdemOkurDE/TestNG_Project01.git
+git push -u origin main
+ */
